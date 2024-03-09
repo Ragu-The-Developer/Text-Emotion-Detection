2cb90fa3161cb459247da9f7c77b7a3ced0cde5e
Ragu-The-Developer <ragugithub@gmail.com>
2024-03-09 13:32:35 +0530

    Succes

--- Text Emotion Detection/app.py
+++ Text Emotion Detection/app.py
@@ -5,16 +5,15 @@ import numpy as np
 import altair as alt
 
 import joblib
+
 pipe_lr = joblib.load(open("model/text_emotion.pkl", "rb"))
 
 emotions_emoji_dict = {"anger": "😠", "disgust": "🤮", "fear": "😨😱", "happy": "🤗", "joy": "😂", "neutral": "😐", "sad": "😔",
                        "sadness": "😔", "shame": "😳", "surprise": "😮"}
 
-
 def predict_emotions(docx):
     results = pipe_lr.predict([docx])
     return results[0]
-
 
 def get_prediction_proba(docx):
     results = pipe_lr.predict_proba([docx])
@@ -54,10 +53,6 @@ def main():
 
             fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
             st.altair_chart(fig, use_container_width=True)
-
-
-
-
 
 
 if __name__ == '__main__':
